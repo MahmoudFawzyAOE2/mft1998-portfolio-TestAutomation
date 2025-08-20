e535b46aff29732fc98a65b087c2baed5b454200
Mahmoud Fawzy Taha-Elaraby <1998mft1998@gmail.com>
2025-08-20 14:26:55 +0300

    add headless mode

--- src/main/java/drivers/DriverManager.java
+++ src/main/java/drivers/DriverManager.java
@@ -4,6 +4,7 @@ package drivers;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.time.Duration;
 
@@ -13,9 +14,34 @@ public class DriverManager {
     public static WebDriver getDriver() {
         if (driver == null) {
             WebDriverManager.chromedriver().setup();
-            driver = new ChromeDriver();
+
+            ChromeOptions options = new ChromeOptions();
+
+            // allow connections from chrome versions that may
+            options.addArguments("--remote-allow-origins=*");
+
+            // Always ensure unique profile directory
+            options.addArguments("--user-data-dir="
+                    + System.getProperty("java.io.tmpdir")
+                    + "/chrome-" + System.currentTimeMillis());
+
+            // Headless mode when specified in cmd prompt
+            // use headless mode by adding -Dheadless=true to cmd
+            String headless = System.getProperty("headless", "false");
+            if (headless.equalsIgnoreCase("true")) {
+                // no UI
+                options.addArguments("--headless=new");
+
+                // needed to launch chrome on root dir
+                options.addArguments("--no-sandbox");
+
+                // disable shared memory usage that may cause crashes
+                options.addArguments("--disable-dev-shm-usage");
+            }
+
+            driver = new ChromeDriver(options);
             driver.manage().window().maximize();
-            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20)); // or read from config
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
         }
         return driver;
     }
