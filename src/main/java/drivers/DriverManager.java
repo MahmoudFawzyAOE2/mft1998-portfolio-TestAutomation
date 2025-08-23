2f4c7af911cbc7fb313a95f913a88523b64b1e1f
Mahmoud Fawzy Taha-Elaraby <1998mft1998@gmail.com>
2025-08-23 20:06:57 +0300

    Revert "--headless=new >> --headless"
    This reverts commit ea152ef21d6f949b88414b3b26de3704cfa22e2d.

--- src/main/java/drivers/DriverManager.java
+++ src/main/java/drivers/DriverManager.java
@@ -33,7 +33,7 @@ public class DriverManager {
                 System.out.println("Headless Mode Activated");
 
                 // no UI
-                options.addArguments("--headless");
+                options.addArguments("--headless=new");
 
                 // needed to launch chrome on root dir
                 options.addArguments("--no-sandbox");
